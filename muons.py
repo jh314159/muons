9ee217261083a66e7bece4548a115055b1b06aeb
Joanna Huang <ucaphua@ucl.ac.uk>
2017-11-10 12:37:03 +0000

    Create TLorentzVector for first lepton

--- muons.py
+++ muons.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-from ROOT import TChain
+from ROOT import TChain, TLorentzVector
 
 data = TChain("mini")
 data.Add("http://opendata.atlas.cern/release/samples/Data/DataMuons.root")
@@ -20,5 +20,16 @@ for i in range(num_events):
 		print "Number of leptons for event ", i, " is ", n_leptons
 		assert(n_leptons==2)
 		pt1 = data.lep_pt[0] # Get pt for leptons
+		eta1 = data.lep_eta[0]
+		phi1 = data.lep_phi[0]
+		E1 = data.lep_E[0]
 		pt2 = data.lep_pt[1]			
 		print "Lepton pts are:", pt1, " and ", pt2
+		p1 = TLorentzVector()
+		p1.SetPtEtaPhiE(pt1, eta1, phi1, E1)
+		print "First lepton pt from vector", p1.Pt()
+		print "-------------------------------------------------------"
+
+
+
+

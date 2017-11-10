a0f33a6529ff112db5a9b23c964845096c7fe223
Joanna Huang <ucaphua@ucl.ac.uk>
2017-11-10 12:21:22 +0000

    Print pts of leptons.

--- muons.py
+++ muons.py
@@ -9,13 +9,16 @@ num_events = data.GetEntries()
 print ("Number of events: "), num_events
 
 # Number of events to process
-num_events = 1000
+num_events = 100
 for i in range(num_events):
 	# Read in the entry
 	data.GetEntry(i)
 	
 	# Get number of leptons for the event
 	n_leptons = data.lep_n
-	if n_leptons >= 2:
+	if n_leptons >= 2: # Look for pairs
 		print "Number of leptons for event ", i, " is ", n_leptons
-
+		assert(n_leptons==2)
+		pt1 = data.lep_pt[0] # Get pt for leptons
+		pt2 = data.lep_pt[1]			
+		print "Lepton pts are:", pt1, " and ", pt2
